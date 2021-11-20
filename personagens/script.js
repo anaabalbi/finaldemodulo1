d09545357dc1bcf42992f10da27697041efa2fb1
anaabalbi <anaabalbi@gmail.com>
2021-11-20 14:19:57 -0300

    personagens

--- personagens/script.js
+++ personagens/script.js
@@ -1,5 +1,20 @@
     
  setTimeout(function comecar(){
     alert('Vamos começar!');
-    var personagem = prompt ('Escolha o seu personagem? \nShikamuro \nIno \nChijo')} 
-, 1500);      
+    var personagem = prompt ('Escolha o seu personagem? \nShikamuro \nIno \nChijo');
+    return personagem;    
+    }, 1500);  
+var personagem = comecar()
+function select(personagem){
+    if(personagem=='shikamuro'){
+        
+
+    }
+    else if(personagem=='Ino'){
+
+
+    }
+    else{
+
+    }
+}
