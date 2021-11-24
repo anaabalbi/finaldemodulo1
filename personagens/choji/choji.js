c639b0314151389e4641acba2c6b2a7f439e0bb1
anaabalbi <anaabalbi@gmail.com>
2021-11-24 16:54:46 -0300

    javascript

---
+++ personagens/choji/choji.js
@@ -0,0 +1,48 @@
+function enigmaChijo1(){
+    var resposta = prompt('Entre com a resposta correta. \n Exemplo de resposta: e')
+    
+    if(resposta==='b'){
+        window.location.href="chojiFase3.html"
+                    
+    }
+    else{
+        window.location.href="../../game over/gameover.html"
+                    
+    }
+
+}
+    
+            
+
+
+function enigmaChoji2(){
+    var resposta = prompt('Entre com a resposta correta')
+
+    if(resposta==20){
+        window.location.href="chojiFase4.html"
+
+    }
+    else {
+                    
+
+        window.location.href="../../game over/gameover.html"
+                    
+    }
+
+}
+
+function enigmaChoji3(){
+    var resposta = prompt('Entre com a resposta correta')
+
+    if(resposta==1){
+        window.location.href="../../sucesso/sucesso.html"
+
+    }
+    else {
+                    
+
+        window.location.href="../../game over/gameover.html"
+                    
+    }
+
+}
